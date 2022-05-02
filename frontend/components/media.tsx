63784af2ed833fe44c671515ffe74f639575d1f9
Adam Coard <arcoard+github@gmail.com>
2022-05-02 10:23:43 -0700

    Demo only, hardcode strapi url for video

--- frontend/components/media.tsx
+++ frontend/components/media.tsx
@@ -1,20 +1,22 @@
 import { Media, StrapiResponseBody } from '../lib/api'
 import styles from '../styles/media.module.css'
 
-export default function HelpMedia({ mediaData }: Media) {
-
-    console.log('helpMedia', { mediaData, conditional: !mediaData })
+export default function HelpMedia({ mediaData }) {
 
     // TODO ONLY TEMPORARY FOR DEMO
     // STRAPI NOT VIABLE FOR HOSTING ON OPENSHIFT
-    function generateStrapiVideoLink(data){
-        return process.env.STRAPI_API_URL + data.url;
+    function generateStrapiVideoLink(data: Media){
+        // TODO - FIX THIS FOR OPENSHIFT.
+        // return process.env.STRAPI_API_URL + data.url; // THIS FAILS, as STRAPI_API_URL is not accessible client-side.  Hardcode for demo
+        // return 'https://localhost:1337' + data.url;
+        // return 'http://localhost:1337/uploads/example_bunny_56f1881a8f.mp4'
+        return 'http://localhost:1337' + data.url;
     }
 
     if (!mediaData) {
         return (
             <div className={styles.missingMedia}>
-                <code>No media in help-media</code>
+                <code>No media for article</code>
             </div>
         )
     }
