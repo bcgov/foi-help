3fd597bfae3cb308a8069d1ede03918e4d379630
Adam Coard <arcoard+github@gmail.com>
2022-07-14 11:26:37 -0700

    Update video player with s3 videos

--- frontend/components/media.tsx
+++ frontend/components/media.tsx
@@ -3,31 +3,28 @@ import styles from '../styles/media.module.css'
 
 export default function HelpMedia({ mediaData }) {
 
-    // TODO ONLY TEMPORARY FOR DEMO
-    // STRAPI NOT VIABLE FOR HOSTING ON OPENSHIFT
+    function isMimeType(type): boolean {
+        return mediaData.attributes.mime.includes(type)
+    }
+
     function generateStrapiVideoLink(data: Media){
-        // TODO - FIX THIS FOR OPENSHIFT.
-        // return process.env.STRAPI_API_URL + data.url; // THIS FAILS, as STRAPI_API_URL is not accessible client-side.  Hardcode for demo
-        // return 'https://localhost:1337' + data.url;
-        // return 'http://localhost:1337/uploads/example_bunny_56f1881a8f.mp4'
-        return 'http://localhost:1337' + data.url;
+        return data.url
     }
 
-    if (!mediaData) {
-        return;
-        // return (
-        //     <div className={styles.missingMedia}>
-        //         <code>No media for article</code>
-        //     </div>
-        // )
+    if (!mediaData || !isMimeType('video')) {
+        return (
+            <div className={styles.missingMedia}>
+                <code>No video for article</code>
+            </div>
+        )
     }
 
+   if (isMimeType('video')) {
+       return (
+           <div className={styles.media}>
+               <video controls className={styles.player} src={generateStrapiVideoLink(mediaData.attributes)}></video>
+           </div>
+       )
+   } 
 
-
-    return (
-        <div className={styles.media}>
-            <video controls className={styles.player} src={generateStrapiVideoLink(mediaData)}></video>
-            {/* Media: <code>{JSON.stringify(mediaData)}</code> */}
-        </div>
-    )
 }
