19bb92f67978a71c6ec168f866b4db8602d59ba5
Adam Coard <arcoard+github@gmail.com>
2022-07-14 10:49:54 -0700

    S3 storage working

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -5,10 +5,15 @@ module.exports = ({ env }) => ({
         providerOptions: {
           accessKeyId: env('FOI_S3_ACCESS_KEY_ID'),
           secretAccessKey: env('FOI_S3_ACCESS_SECRET_KEY'),
-        //   region: env('AWS_REGION'),
+          region: 'us-east',
+          endpoint: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`, 
           params: {
-            Bucket: env('FOI_S3_BUCKETNAME'),
+            // Bucket: env('FOI_S3_BUCKETNAME'),
+            Bucket: 'strapi-help',
           },
+          // Required for BCGov S3 due to different endpoint structure from AWS
+          // Instead of using subdomains, the bucket is a path.
+          s3BucketEndpoint: true,
         },
         actionOptions: {
           upload: {},
@@ -19,3 +24,4 @@ module.exports = ({ env }) => ({
     },
     // ...
   });
+
