71ea7d90df1c92d460bd0b2a22e1e3ff8264351a
Adam Coard <arcoard+github@gmail.com>
2022-08-08 10:26:32 -0700

    Fixed build time import path error

--- frontend/pages/api/restricted.js
+++ frontend/pages/api/restricted.js
@@ -1,5 +1,6 @@
 import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]"
+// import { authOptions } from "./api/auth/[...nextauth]"
+import { authOptions } from "./auth/[...nextauth]"
 
 export default async (req, res) => {
   const session = await unstable_getServerSession(req, res, authOptions)
