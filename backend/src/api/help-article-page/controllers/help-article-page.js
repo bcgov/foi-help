9b6977b77a3ef04eef5315b12665eb8ed32fd430
Adam Coard <adam.coard@aot-technologies.com>
2022-10-27 08:46:00 -0700

    Save first draft of updated schema

---
+++ backend/src/api/help-article-page/controllers/help-article-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  help-article-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::help-article-page.help-article-page');
