40191f734f77dedbf8d13b9ab64c013c5b1a6fe2
Richard Qi <richard.qi@aot-technologies.com>
2025-03-13 23:15:32 -0700

    not using s3Options

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,22 +3,20 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          s3Options: {
-            credentials: {
-              accessKeyId: env('FOI_S3_ACCESS_KEY_ID'),
-              secretAccessKey: env('FOI_S3_ACCESS_SECRET_KEY'),
-            },
-            region: 'us-east',
-            params: {
-              Bucket: env('FOI_S3_BUCKETNAME'),
-              // Bucket: 'strapi-help',
-            },
-            baseUrl: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
-            // // Required for BCGov S3 due to different endpoint structure from AWS
-            // // Instead of using subdomains, the bucket is a path.
-            // s3BucketEndpoint: true,
-            // s3ForcePathStyle: true,
+          credentials: {
+            accessKeyId: env('FOI_S3_ACCESS_KEY_ID'),
+            secretAccessKey: env('FOI_S3_ACCESS_SECRET_KEY'),
           },
+          region: 'us-east-1',
+          params: {
+            Bucket: env('FOI_S3_BUCKETNAME'),
+            // Bucket: 'strapi-help',
+          },
+          endpoint: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
+          // // Required for BCGov S3 due to different endpoint structure from AWS
+          // // Instead of using subdomains, the bucket is a path.
+          s3BucketEndpoint: true,
+          s3ForcePathStyle: true,
         },
         actionOptions: {
           upload: {},
