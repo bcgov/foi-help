4ceaf3358cf11a579102e7670f7f3c5af1343105
Richard Qi <richard.qi@aot-technologies.com>
2025-03-14 09:17:56 -0700

    re-apply the fix

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,18 +3,20 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          credentials: {
-            accessKeyId: env('FOI_S3_ACCESS_KEY_ID'),
-            secretAccessKey: env('FOI_S3_ACCESS_SECRET_KEY'),
-          },
-          region: 'us-east-1',
-          params: {
-            // Bucket: env('FOI_S3_BUCKETNAME'),
-            // Bucket: 'strapi-help',
+          s3Options: {
+            credentials: {
+              accessKeyId: env('FOI_S3_ACCESS_KEY_ID'),
+              secretAccessKey: env('FOI_S3_ACCESS_SECRET_KEY'),
+            },
+            region: 'us-east-1',
+            params: {
+              Bucket: env('FOI_S3_BUCKETNAME'),
+              // Bucket: 'strapi-help',
+            },
           },
           endpoint: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
-          // // Required for BCGov S3 due to different endpoint structure from AWS
-          // // Instead of using subdomains, the bucket is a path.
+          // Required for BCGov S3 due to different endpoint structure from AWS
+          // Instead of using subdomains, the bucket is a path.
           s3BucketEndpoint: true,
           s3ForcePathStyle: true,
         },
