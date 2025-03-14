90f709c06bf8008e18b46fd2edb826aa2268b57b
Richard Qi <richard.qi@aot-technologies.com>
2025-03-14 09:45:15 -0700

    move all settings inside s3Options

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -13,12 +13,13 @@ module.exports = ({ env }) => ({
               Bucket: env('FOI_S3_BUCKETNAME'),
               // Bucket: 'strapi-help',
             },
+            baseUrl: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
+            endpoint: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
+            // Required for BCGov S3 due to different endpoint structure from AWS
+            // Instead of using subdomains, the bucket is a path.
+            s3BucketEndpoint: true,
+            s3ForcePathStyle: true,
           },
-          endpoint: `https://${env('FOI_S3_HOST')}/${env('FOI_S3_BUCKETNAME')}`,
-          // Required for BCGov S3 due to different endpoint structure from AWS
-          // Instead of using subdomains, the bucket is a path.
-          s3BucketEndpoint: true,
-          s3ForcePathStyle: true,
         },
         actionOptions: {
           upload: {},
